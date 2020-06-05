770a801161c867b1fce515bdc0dad22068032ad5
gavinsimon1 <gavin.simon808@gmail.com>
2020-06-04 20:29:57 -1000

    updated forecast.js

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -12,7 +12,7 @@ const forecast = (latitude, longitude, callback) => {
         } else if(body.error) {
             callback('Unable to find location.', undefined)
         } else {
-          callback(undefined, "The weather is " + body.current.weather_descriptions + ".  The temperature is " + body.current.temperature + " degrees.  It feels like " + body.current.feelslike + " degrees." )
+          callback(undefined, "The weather is " + body.current.weather_descriptions + ".  The temperature is " + body.current.temperature + " degrees.  It feels like " + body.current.feelslike + " degrees.  The humidity is " + body.current.humidity + ".  The wind speed is " + body.current.wind_speed + ".  The UV index is " + body.current.uv_index + "." )
           }
         })
     }
